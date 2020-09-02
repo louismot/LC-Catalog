a32076c4f6e732fdd8c42c7ddd5c46d4ac8ab61b
louismot <louismot@buffalo.edu>
2020-09-02 09:18:07 -0400

    installed missing dependencies, installed & implemented axios for data query, made basic table

---
+++ lc-catalog/src/components/BookTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <table class="table">
+    <thead>
+
+    <tr>
+      <th>Level</th>
+      <th>Title</th>
+      <th>Author</th>
+      <th>Available (y/n)</th>
+    </tr>
+
+    </thead>
+    <tbody>
+
+    <tr>
+      <td>K</td>
+      <td>Amazing Title</td>
+      <td>Some Author</td>
+      <td>y</td>
+    </tr>
+
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "BookTable"
+}
+</script>
+
+<style scoped>
+
+</style>
